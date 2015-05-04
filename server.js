c38741f72221cb9b44fc1c84e2adcb71c2e663c7
Emre Surmeli <s.emre.s.8@gmail.com>
2015-05-03 23:29:20 -0700

    enough for tonight

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var http = require("http");
+var url = require("url");
 
-function onRequest(req, res) {
-  console.log("Request recieved.");
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.write("Hello World");
-  res.end();
+function start(route) {
+  function onRequest(req, res) {
+    var pathname = url.parse(req.url).pathname;
+    console.log("Request for " + pathname + " recieved.");
+
+    route(pathname);  
+
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write("Hello World");
+    res.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log("Server has started.");
+exports.start = start;
